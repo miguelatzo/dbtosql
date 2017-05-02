70349b28aa2b600c5b144c62467205c3a048f9b8
miguelatzo <gersonzarate@gmail.com>
2017-05-02 07:48:07 -0500

    add sqlStr creation and .sql file creation

--- ETLG/src/etlg/fileOpener.java
+++ ETLG/src/etlg/fileOpener.java
@@ -8,21 +8,29 @@ package etlg;
  *
  * @author mike
  */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class fileOpener {
     
     private static File dbFile;
-    private JFileChooser fileChooser;
+    private static JFileChooser fileChooser;
     
     public fileOpener(){
-        this.fileChooser = new JFileChooser();
-        this.fileChooser.setFileFilter(new FileNameExtensionFilter("MS Office Documents", "accdb", "mdb"));
+        fileOpener.fileChooser = new JFileChooser();
+        fileOpener.fileChooser.setFileFilter(new FileNameExtensionFilter("MS Office Documents", "accdb", "mdb"));
     }
     public void setDBFile(){
-        int fileChooserResult = this.fileChooser.showOpenDialog(null);//el resultado de abrir el cuadro de dialogo para abrir archivos
+        int fileChooserResult = fileOpener.fileChooser.showOpenDialog(null);//el resultado de abrir el cuadro de dialogo para abrir archivos
         if(fileChooserResult == JFileChooser.APPROVE_OPTION){//si es exitosa
             fileOpener.dbFile = fileChooser.getSelectedFile();   
         }
@@ -31,5 +39,26 @@ public class fileOpener {
     public static File getDBFile(){
         return fileOpener.dbFile;
     }
-    
+    private static File archivo; //el archivo utilizado para escribir sobre él o para cargar un archivo existente sobre él
+    private static BufferedWriter bw;//el buffer para la escritura de lineas
+    private static int resultado;//integer para verificar si el archivo se abrió o guardó con éxito
+    //cadena guarda las líneas leídas, línea guarda la linea que el filereader regresa, archivoreciente almacena el nombre del útimo archivo compilado
+    private static String cadena = "";
+    public static void guardarArchivo(String sqlStr){
+        fileOpener.cadena = JOptionPane.showInputDialog("nombre del archivo");//se pregunta por el nombre del archivo
+        fileOpener.archivo = new File(fileOpener.cadena+".sql");//se crea un nuevo archivo con extensión .txt y se almacena en archivo
+        fileOpener.fileChooser.setSelectedFile(fileOpener.archivo);//se hace highlight/selecciona sobre el archivo para su posterior escritura 
+        
+        fileOpener.resultado = fileOpener.fileChooser.showSaveDialog(null);//el resultado indica si fue exitosa la operación
+        if(fileOpener.resultado == JFileChooser.APPROVE_OPTION){//si fue exitosa entonces...
+            try {
+                fileOpener.bw = new BufferedWriter(new FileWriter(fileOpener.fileChooser.getSelectedFile()));//se inicia el buffer de escritura para el archivo creado
+                fileOpener.bw.write(sqlStr); //Se escribe el contenido de Area1 en el archivo
+                fileOpener.bw.close();//se desecha el buffer de escritura
+                JOptionPane.showMessageDialog(null, "El archivo se ha guardado","File Saved",JOptionPane.INFORMATION_MESSAGE);
+            } catch (Exception ex) {
+                Logger.getLogger(fileOpener.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 }
--- ETLG/src/etlg/tableView.java
+++ ETLG/src/etlg/tableView.java
@@ -7,6 +7,8 @@ package etlg;
 import com.healthmarketscience.jackcess.Column;
 import com.healthmarketscience.jackcess.Row;
 import com.healthmarketscience.jackcess.Table;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -21,9 +23,11 @@ public class tableView extends JFrame{
     private Table table;
     private JScrollPane jScrollPane1;
     private JTable jTable1;
+    private JButton jButton1;
     Object[][] data;
     String[] columnNames;
-    String sqlStr = "CREATE DATABASE x;\n";
+    
+    static String sqlStr = "CREATE DATABASE x;\n";
     /** Creates new form tableViewer */
     
     public tableView(){
@@ -33,7 +37,6 @@ public class tableView extends JFrame{
         this.table = this.database.getGenericTable();
         setJackcessTable();
         initComponents();
-        makeInsert();
     }
     
     private databaseMDB getMDBdatabase(){
@@ -59,29 +62,52 @@ public class tableView extends JFrame{
             for(Column column : this.table.getColumns()) {
                 this.columnNames[j] = column.getName().toString();
                 this.data[i][j] = row.get(column.getName().toString());
-                System.out.println(data[i][j]);
                 ++j;
             }
             ++i;
         }
     }
-        
-    public void makeTable(){
+    
+    
+    private void makeTable(){
         sqlStr += "CREATE TABLE "+this.tableName+" (";
         for (Column column : this.table.getColumns()) {
-            sqlStr += column.getName().toString()+" varchar(40),";
+            sqlStr += column.getName().toString()+" varchar(40),\n";
         }
         sqlStr = sqlStr.substring(0, sqlStr.length() - 1);
         sqlStr += ");";
     }
     
-    public void makeInsert(){
-        System.out.println(jTable1.getAccessibleContext().getAccessibleTable().getAccessibleAt(1, 1).toString());
+    private void makeInserts(){
+        sqlStr += "INSERT INTO "+this.tableName+" VALUES\n";
+        for (int i = 0; i < this.table.getRowCount(); i++) {
+            sqlStr += "(";
+            for (int j = 0; j < this.table.getColumnCount(); j++) {
+                sqlStr += jTable1.getValueAt(i, j)+",";
+            }
+            sqlStr = sqlStr.substring( 0, sqlStr.length() - 1 );
+            sqlStr += "),\n";
+        }
+        sqlStr = sqlStr.substring( 0, sqlStr.length() - 1 );
+        sqlStr += ";";
+    }
+    private void jButton1ActionPerformed(ActionEvent evt) {
+        makeTable();
+        makeInserts();
+        fileOpener.guardarArchivo(sqlStr);
+        sqlStr = "";
     }
     private void initComponents() {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-
+        jButton1 = new javax.swing.JButton();
+        jButton1.setText("Crear Tabla");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(this.data, this.columnNames));
@@ -95,10 +121,17 @@ public class tableView extends JFrame{
                 .addContainerGap(35, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 452, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(22, 22, 22))
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(426, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addComponent(jButton1)
+                .addGap(18, 18, 18)
                 .addContainerGap(25, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 369, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
